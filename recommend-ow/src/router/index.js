2994b956fbe06ddc0a5b69ac8581f4fdde3449da
BaleX <blank@blank.com>
2026-02-26 20:12:17 +0100

    feat: configure routes for dashboard and show details

--- recommend-ow/src/router/index.js
+++ recommend-ow/src/router/index.js
@@ -1,8 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import DashboardView from '@/features/shows/views/DashboardView.vue';
+import ShowsDetailsView from '@/features/shows/views/ShowsDetailsView.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: DashboardView,
+    },
+    {
+        path: '/show/:id',
+        name: 'ShowDetails',
+        component: ShowsDetailsView,
+    },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
 
-export default router
+export default router;
